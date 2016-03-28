919372256f5a03931638570e62a712ee5af5831e
Kakarot <disgaea@outlook.com>
2016-03-28 12:44:46 -0400

    Created new Patient in FHIR database, then copy/pasted the JSON into Patient.JSON and wrote unit tests as example code for querying basic information from JSON.

---
+++ EhrgoHealth.WebService.UnitTests/AllergyIntoleranceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Collections.Generic;
+
+namespace EhrgoHealth.WebService.UnitTests
+{
+    [TestClass]
+    public class AllergyIntoleranceTest
+    {
+        [TestMethod]
+        public void TestReadPatientName()
+        {
+            using (StreamReader r = new StreamReader(@"..\..\Patient.json"))
+            {
+                string json = r.ReadToEnd();
+                Console.WriteLine(json);
+               // List<Item> items = JsonConvert.DeserializeObject<List<Item>>(json);
+            }
+        }
+    }
+}
---
+++ EhrgoHealth.WebService.UnitTests/JSONParserTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace EhrgoHealth.WebService.UnitTests
+{
+    [TestClass]
+    public class JSONParserTest
+    {
+        static JObject patientJSON;
+
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            patientJSON = JObject.Parse(File.ReadAllText(@"..\..\Patient.json"));
+        }
+
+        [TestMethod]
+        public void TestReadPatientGender()
+        {         
+            Assert.AreEqual("male", patientJSON.GetValue("gender"));
+        }
+
+        [TestMethod]
+        public void TestReadPatientBirthDate()
+        {
+            Assert.AreEqual("1974-12-25", patientJSON.GetValue("birthDate"));
+        }
+    }
+}
