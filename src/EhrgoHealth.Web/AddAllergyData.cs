6be8b42a30a15a81bd2fd60c5a15437e641fd8de
Tommy Parnell <tparnell8@gmail.com>
2016-04-30 19:24:16 -0400

    add getallergylist

--- src/EhrgoHealth.Web/AddAllergyData.cs
+++ src/EhrgoHealth.Web/AddAllergyData.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Owin.Security;
+using Owin.Security.Providers.Fitbit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +10,30 @@ namespace EhrgoHealth.Web
 {
     public class AddAllergyData
     {
+        private ApplicationUserManager userManager;
+        private IAuthenticationManager authManager;
+
+        public AddAllergyData()
+        {
+        }
+
+        public AddAllergyData(ApplicationUserManager userManager, IAuthenticationManager authManager)
+        {
+            this.userManager = userManager;
+            this.authManager = authManager;
+        }
+
         public void AddAllergyToMedication(string fhirPatientId, string medicationName)
         {
         }
 
         public void RemoveAllergyToMedication(string fhirPatientId, string medicationName)
         {
+        }
+
+        public IEnumerable<string> GetAllergyList()
+        {
+            return Enumerable.Empty<string>();
         }
     }
 }
