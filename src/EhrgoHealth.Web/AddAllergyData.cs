47f1d9b7db006f584fbe2a045e60d4b70cfab52c
Tommy Parnell <tparnell8@gmail.com>
2016-04-30 19:20:53 -0400

    add allerydatacontract

---
+++ src/EhrgoHealth.Web/AddAllergyData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EhrgoHealth.Web
+{
+    public class AddAllergyData
+    {
+        public void AddAllergyToMedication(string fhirPatientId, string medicationName)
+        {
+        }
+
+        public void RemoveAllergyToMedication(string fhirPatientId, string medicationName)
+        {
+        }
+    }
+}
